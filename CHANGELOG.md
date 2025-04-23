9dab8c8ab9fb254d67d1e1f054d8c46ed2e023c6
vzagorovskiy <vzagorovskiy@gmail.com>
2025-04-23 15:06:45 +0300

    По умолчанию запрашивается счет за прошлый месяц

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,22 @@
 # Changelog
 
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+
 ## [1.5.0] - 2025-04-23
 
 ### Added
 
- - Поддержка получения счетов за предыдущие месяцы (на указанную дату) и за текущий месяц (по умолчанию).
+ - Поддержка получения счетов на указанную дату (месяц) и за прошлый месяц (по умолчанию).
 
 ### Updated
 
  - Исправлена документация
  - Добавлен пример запроса счета на указанную дату
+ - Добавлен пример запроса счета за текущий месяц
 
 ## [1.4.1] - 2025-03-21
 
--- custom_components/mygas/services.py
+++ custom_components/mygas/services.py
@@ -29,7 +29,7 @@ from .const import (
     SERVICE_SEND_READINGS,
 )
 from .coordinator import MyGasCoordinator
-from .helpers import async_get_coordinator, get_bill_date, get_float_value
+from .helpers import async_get_coordinator, get_bill_date, get_float_value, get_previous_month
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -119,7 +119,7 @@ async def _async_handle_get_bill(
     hass: HomeAssistant, service_call: ServiceCall, coordinator: MyGasCoordinator
 ) -> dict[str, Any]:
     device_id = service_call.data.get(ATTR_DEVICE_ID)
-    bill_date = service_call.data.get(ATTR_DATE, get_bill_date())
+    bill_date = service_call.data.get(ATTR_DATE, get_previous_month())
     email = service_call.data.get(ATTR_EMAIL)
 
     result = await coordinator.async_get_bill(device_id, bill_date, email)
